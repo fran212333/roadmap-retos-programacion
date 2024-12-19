79608ce7c43aa26018866902e5bde4ff47e1dffe
fran212333 <francisco.gr0526@gmail.com>
2024-12-19 11:03:52 -0600

    Create Fran212333.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Fran212333.py
@@ -0,0 +1,26 @@
+# este es un comentario en una linea
+
+# :/python.org
+
+"""
+esto es un comentario 
+en varias lineas 
+xd
+"""
+
+mi_Variable = "mi Variable"
+mi_Variable = "valor de la variable"
+
+MI_CONSTANTE ="mi constante"  # esto funiona por convercion 
+
+mi_int = 3
+my_float = 2.3
+my_bool = True 
+my_bool = False
+my_string = "esto es texto"
+my_other_string = "otro texto xd"
+
+print("¡Hola, python!")
+
+
+
